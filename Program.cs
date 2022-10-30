9d2e8da0d3d93ee24ee168a872e9b9c1cdfe2850
Declan Cross <dj3dxx@gmail.com>
2022-10-30 14:19:42 +1300

    Refresh github pages and improved comments and ui

--- Program.cs
+++ Program.cs
@@ -11,9 +11,13 @@ namespace Woot_verlay
 {
     internal static class Program
     {
+        // global variables
+        public static bool runSystem = true;
         public static List<TcpClient> activeConnections = new List<TcpClient>();
+
+
         /// <summary>
-        ///  The main entry point for the application.
+        ///  The entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
@@ -22,48 +26,43 @@ namespace Woot_verlay
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
            
-
-            var keyboardReceiver = new KeyListener();
-            var inputSource = new InputSource(keyboardReceiver);
-
-            // Starts listening for input
-            inputSource.Listen();
-
-            // Load WootingAnalogSDK
-            Console.WriteLine("Woot-verlay launched!\n");
-
-            // Initialise the SDK
+            
+            // load WootingAnalogSDK
             var (noDevices, error) = WootingAnalogSDK.Initialise();
-            // If the number of devices is at least 0 it indicates the initialisation was successful
-            if (noDevices >= 0)
+            // print error if no Wooting devices found
+            if (noDevices < 0)
             {
-                Console.WriteLine($"\nAnalog SDK Successfully initialised with {noDevices} devices!");
-            }
-            else
-            {
-                Console.WriteLine($"\nAnalog SDK failed to initialise: {error}");
-                MessageBox.Show("It seems that there are no Wooting devices connected. \nPlease plug in your keyboard and try again!", "Woot-verlay - no device error");
+                MessageBox.Show("It seems that there are no Wooting devices connected. \nPlease plug in your keyboard and try again!\nAnalog SDK failed to initialise: {error}", "Woot-verlay - no device error");
                 System.Environment.Exit(1);
             }
+
+            // create keyboard hooks and listen for input
+            var keyboardReceiver = new KeyListener();
+            var inputSource = new InputSource(keyboardReceiver);
+            inputSource.Listen();
 
             // initialise server
             string ip = "127.0.0.1";
             int port = 80;
             var server = new TcpListener(IPAddress.Parse(ip), port);
             server.Start();
-            Console.WriteLine("\nServer has started on {0}:{1}, Waiting for a connection…\n", ip, port);
+            //Console.WriteLine("\nServer has started on {0}:{1}, Waiting for a connection…\n", ip, port);
 
+            // run client handler and main program loop
             ThreadPool.QueueUserWorkItem(o => handleClients(server));
             ThreadPool.QueueUserWorkItem(o => runLoop(keyboardReceiver));
 
-            // Start application
+            // start application
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new WootTrayApp());
-
         }
 
-        private static void SendEcho(NetworkStream stream, string inputText)
+
+        /// <summary>
+        /// Sends a message to a JavaSript websocket
+        /// </summary>
+        private static void sendMessage(NetworkStream stream, string inputText)
         {
 
             byte[] sendBytes = Encoding.UTF8.GetBytes(inputText);
@@ -107,9 +106,13 @@ namespace Woot_verlay
             stream.Write(responseArray.ToArray(), 0, responseArray.Count);
         }
 
+
+        /// <summary>
+        /// Looping method to accept incoming connections
+        /// </summary>
         public static void handleClients(TcpListener server)
         {
-            while (true)
+            while (runSystem)
             {
                 TcpClient client = server.AcceptTcpClient();
                 NetworkStream stream = client.GetStream();
@@ -125,7 +128,7 @@ namespace Woot_verlay
                     // provide first link handshake
                     if (Regex.IsMatch(s, "^GET", RegexOptions.IgnoreCase))
                     {
-                        Console.WriteLine("=====Handshaking from client=====\n{0}==============================\n", s);
+                        //Console.WriteLine("=====Handshaking from client=====\n{0}==============================\n", s);
                         string swk = Regex.Match(s, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
                         string swka = swk + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
                         byte[] swkaSha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(swka));
@@ -141,7 +144,7 @@ namespace Woot_verlay
                         stream.Write(response, 0, response.Length);
                         hasHandshaked = true;
                         activeConnections.Add(client);
-                        Console.WriteLine("Client " + activeConnections.Count + " connected.\n");
+                        //Console.WriteLine("Client " + activeConnections.Count + " connected.\n");
                     }
                 }
 
@@ -149,12 +152,16 @@ namespace Woot_verlay
             }
         }
 
+
+        /// <summary>
+        /// Main program loop that reads Wooting SDK input and sends info to clients
+        /// </summary>
         public static void runLoop(KeyListener keyboardReceiver) {
             // run SDK loop
             bool shownEmpty = false;
-            while (true)
+            while (runSystem)
             {
-
+                // remove inactive clients
                 List<TcpClient> disconnected = activeConnections.FindAll(curClient => !curClient.Connected);
                 if (disconnected.Count > 0)
                 {
@@ -162,48 +169,42 @@ namespace Woot_verlay
                     disconnected.ForEach(client => activeConnections.Remove(client));
                 }
 
+                // read analogue buffer and start sending keys
                 var (keys, readErr) = WootingAnalogSDK.ReadFullBuffer(20);
-                if (readErr == WootingAnalogResult.Ok)
-                {
+                if (readErr == WootingAnalogResult.Ok) {
                     // collect all key information
-                    try
-                    {
-                        if (keys.Count > 0)
-                        {
+                    try {
+                        if (keys.Count > 0){
                             String content = "";
-                            foreach (var analog in keys)
-                            {
+                            foreach (var analog in keys){
                                 var pressed = (keyboardReceiver.activeKeys.Contains(analog.Item1)) ? 1 : 0;
                                 content += "(" + analog.Item1 + ":" + analog.Item2 + ":" + pressed + ")";
                             }
                             // message info to client for display
-                            activeConnections.ForEach(curClient => SendEcho(curClient.GetStream(), content));
+                            activeConnections.ForEach(curClient => sendMessage(curClient.GetStream(), content));
                             shownEmpty = false;
-
                         }
                         else if (!shownEmpty)
                         {
                             // allow a single empty line after all keys are released
-                            activeConnections.ForEach(curClient => SendEcho(curClient.GetStream(), ""));
+                            activeConnections.ForEach(curClient => sendMessage(curClient.GetStream(), ""));
                             shownEmpty = true;
                         }
                     }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Client unavailable - removing next loop.");
-                    }
+                    catch (Exception) { Console.WriteLine("Client unavailable - removing next loop."); }
                 }
-                else
-                {
-                    Console.WriteLine($"Read failed with {readErr}");
+                else {
+                    //Console.WriteLine($"Read failed with {readErr}");
                     Thread.Sleep(1000);
                 }
-                // We want to have a bit of a delay so we don't spam the console with new values
                 Thread.Sleep(25);
             }
         }
 
 
+        /// <summary>
+        /// Class <c>KeyListener</c> listens to key presses and stores active ones
+        /// </summary>
         internal class KeyListener : IKeyboardEventReceiver
         {
             // convert key events to wooting key numbers
@@ -222,58 +223,62 @@ namespace Woot_verlay
             // store active keys
             public List<int> activeKeys = new List<int>();
 
-            // store actuation points later
-
             // listen key upstrokes and downstrokes
             public void Receive(KeyboardEvent @event)
             {
-                var keyCode = keyMaps.Space;
                 // Console.WriteLine(@event.Key.ToString());
+
+                var keyCode = keyMaps.Space;
                 keyMaps.TryParse(@event.Key.ToString(), out keyCode);
                 if ((int)keyCode > 0)
                 {
                     if (@event.State == NeatInput.Windows.Processing.Keyboard.Enums.KeyStates.Up && activeKeys.Contains((int)keyCode))
-                    {
+                    { // remove key on upstroke
                         activeKeys.Remove((int)keyCode);
                     }
                     else if (!activeKeys.Contains((int)keyCode))
-                    {
+                    { // add key on downstroke
                         activeKeys.Add((int)keyCode);
-
                     }
                 }
             }
         }
 
-        public class WootTrayApp : ApplicationContext
+        /// <summary>
+        /// Class <c>WootTrayApp</c> is the main tray application
+        /// </summary>
+        internal class WootTrayApp : ApplicationContext
         {
             private NotifyIcon trayIcon;
 
             public WootTrayApp()
             {
+                var strip = new ContextMenuStrip()
+                {
+                    Items =
+                        {
+                            new ToolStripMenuItem("Stop overlay", null, new EventHandler(Exit), "EXIT")
+                        }
+                };
+                strip.BackColor = Color.FromArgb(255, 20, 21, 24); ;
+                strip.ForeColor = Color.White;
+                strip.RenderMode = ToolStripRenderMode.System;
+
                 trayIcon = new NotifyIcon()
                 {
                     Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath),
-                    ContextMenuStrip = new ContextMenuStrip()
-                    {
-                        Items =
-                        {
-
-                new ToolStripMenuItem("Stop overlay", null, new EventHandler(Exit), "EXIT")
-            }
-                    },
+                    ContextMenuStrip = strip,
                     Visible = true
                 };
             }
 
-            void Exit(object sender, EventArgs e)
+            void Exit(object? sender, EventArgs e)
             {
                 // Hide tray icon, otherwise it will remain shown until user mouses over it
                 trayIcon.Visible = false;
                 Application.Exit();
             }
         }
-
     }
 
 
