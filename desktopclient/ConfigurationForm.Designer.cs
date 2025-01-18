f8c48eb04df3f381a991f04e7124c6f04e236073
Declan Cross <dj3dxx@gmail.com>
2025-01-18 13:48:55 +1300

    Start initial support for custom ports

--- desktopclient/ConfigurationForm.Designer.cs
+++ desktopclient/ConfigurationForm.Designer.cs
@@ -40,18 +40,19 @@ namespace Woot_verlay
             label1 = new Label();
             label2 = new Label();
             toolTip1 = new ToolTip(components);
+            portinput = new NumericUpDown();
             titlebar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)portinput).BeginInit();
             SuspendLayout();
             // 
             // startbtn
             // 
-            resources.ApplyResources(startbtn, "startbtn");
             startbtn.BackColor = Color.FromArgb(84, 150, 126);
             startbtn.BorderRadius = 45;
             startbtn.Cursor = Cursors.Hand;
+            resources.ApplyResources(startbtn, "startbtn");
             startbtn.ForeColor = Color.White;
             startbtn.Name = "startbtn";
-            toolTip1.SetToolTip(startbtn, resources.GetString("startbtn.ToolTip"));
             startbtn.UseVisualStyleBackColor = false;
             startbtn.Click += startBtn_Click;
             // 
@@ -68,54 +69,50 @@ namespace Woot_verlay
             // 
             // wootingKbSelect
             // 
-            resources.ApplyResources(wootingKbSelect, "wootingKbSelect");
             wootingKbSelect.BackColor = Color.FromArgb(64, 64, 64);
             wootingKbSelect.BorderRadius = 15;
             wootingKbSelect.Cursor = Cursors.Hand;
             wootingKbSelect.FlatAppearance.BorderSize = 0;
             wootingKbSelect.FlatAppearance.MouseDownBackColor = Color.FromArgb(50, 50, 50);
+            resources.ApplyResources(wootingKbSelect, "wootingKbSelect");
             wootingKbSelect.ForeColor = Color.White;
             wootingKbSelect.Name = "wootingKbSelect";
-            toolTip1.SetToolTip(wootingKbSelect, resources.GetString("wootingKbSelect.ToolTip"));
             wootingKbSelect.UseVisualStyleBackColor = false;
             wootingKbSelect.Click += wootingKbSelect_Click;
             // 
             // genericKbSelect
             // 
-            resources.ApplyResources(genericKbSelect, "genericKbSelect");
             genericKbSelect.BackColor = Color.FromArgb(64, 64, 64);
             genericKbSelect.BorderRadius = 15;
             genericKbSelect.Cursor = Cursors.Hand;
             genericKbSelect.FlatAppearance.BorderSize = 0;
             genericKbSelect.FlatAppearance.MouseDownBackColor = Color.FromArgb(50, 50, 50);
+            resources.ApplyResources(genericKbSelect, "genericKbSelect");
             genericKbSelect.ForeColor = Color.White;
             genericKbSelect.Name = "genericKbSelect";
-            toolTip1.SetToolTip(genericKbSelect, resources.GetString("genericKbSelect.ToolTip"));
             genericKbSelect.UseVisualStyleBackColor = false;
             genericKbSelect.Click += genericKbSelect_Click;
             // 
             // closeBtn
             // 
-            resources.ApplyResources(closeBtn, "closeBtn");
             closeBtn.BackColor = Color.Black;
             closeBtn.Cursor = Cursors.Hand;
             closeBtn.FlatAppearance.BorderSize = 0;
             closeBtn.FlatAppearance.MouseDownBackColor = Color.Transparent;
             closeBtn.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            resources.ApplyResources(closeBtn, "closeBtn");
             closeBtn.ForeColor = SystemColors.ControlLight;
             closeBtn.Name = "closeBtn";
-            toolTip1.SetToolTip(closeBtn, resources.GetString("closeBtn.ToolTip"));
             closeBtn.UseVisualStyleBackColor = false;
             closeBtn.Click += closeBtn_Click;
             // 
             // titlebar
             // 
-            resources.ApplyResources(titlebar, "titlebar");
             titlebar.BackColor = Color.Transparent;
             titlebar.Controls.Add(label1);
             titlebar.Controls.Add(closeBtn);
+            resources.ApplyResources(titlebar, "titlebar");
             titlebar.Name = "titlebar";
-            toolTip1.SetToolTip(titlebar, resources.GetString("titlebar.ToolTip"));
             titlebar.MouseDown += TitleBar_MouseDown;
             titlebar.MouseMove += TitleBar_MouseMove;
             titlebar.MouseUp += TitleBar_MouseUp;
@@ -124,18 +121,28 @@ namespace Woot_verlay
             // 
             resources.ApplyResources(label1, "label1");
             label1.Name = "label1";
-            toolTip1.SetToolTip(label1, resources.GetString("label1.ToolTip"));
             // 
             // label2
             // 
             resources.ApplyResources(label2, "label2");
             label2.Name = "label2";
-            toolTip1.SetToolTip(label2, resources.GetString("label2.ToolTip"));
             label2.Click += label2_Click;
             // 
             // toolTip1
             // 
             toolTip1.Popup += toolTip1_Popup;
+            // 
+            // portinput
+            // 
+            portinput.BackColor = Color.FromArgb(64, 64, 64);
+            portinput.BorderStyle = BorderStyle.None;
+            portinput.ForeColor = Color.White;
+            resources.ApplyResources(portinput, "portinput");
+            portinput.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+            portinput.Minimum = new decimal(new int[] { 1024, 0, 0, 0 });
+            portinput.Name = "portinput";
+            portinput.Value = new decimal(new int[] { 32312, 0, 0, 0 });
+            portinput.Visible = false;
             // 
             // SetupForm
             // 
@@ -143,6 +150,7 @@ namespace Woot_verlay
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
             ControlBox = false;
+            Controls.Add(portinput);
             Controls.Add(label2);
             Controls.Add(genericKbSelect);
             Controls.Add(wootingKbSelect);
@@ -156,10 +164,10 @@ namespace Woot_verlay
             MinimizeBox = false;
             Name = "SetupForm";
             SizeGripStyle = SizeGripStyle.Hide;
-            toolTip1.SetToolTip(this, resources.GetString("$this.ToolTip"));
             Load += Form1_Load;
             titlebar.ResumeLayout(false);
             titlebar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)portinput).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -173,6 +181,7 @@ namespace Woot_verlay
         private RoundedButton genericKbSelect;
         private RoundedButton startbtn;
         private ToolTip toolTip1;
+        private NumericUpDown portinput;
     }
 
     public class RoundedButton : Button
