891376cf0832fbdfdeffaf7413074cd5c4718113
Declan Cross <dj3dxx@gmail.com>
2025-01-21 22:49:19 +1300

    Add X and X key labels

--- pages/osulay/osucanvas.js
+++ pages/osulay/osucanvas.js
@@ -11,6 +11,10 @@ var xLevel = 0;
 var prevXLevel = 0;
 var xActive = false;
 
+const activeColour = "#00ff95";
+const inactiveColour = "#ff0000";
+const transparentColour = "#39393976";
+
 // interpolation variables
 var targetZLevel = 0;
 var targetXLevel = 0;
@@ -26,6 +30,7 @@ var scale = 8;
 canvas.width = canvasWidth * scale;
 canvas.height = canvasHeight * scale;
 
+// key display settings
 const zSettings = {
     key: "Z",
     y: 0.33,
@@ -37,6 +42,10 @@ const xSettings = {
     y: 0.66,
     maxHeight: 0.2
 }
+
+// key icon settings
+const zKeyIcon = document.getElementById('zKey');
+const xKeyIcon = document.getElementById('xKey');
 
 ctx.imageSmoothingEnabled = false;
 
@@ -55,9 +64,9 @@ function drawCanvas(){
 
     // Draw line for zLevel
     if(zActive){
-        ctx.strokeStyle = "green";
+        ctx.strokeStyle = activeColour;
     } else {
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = inactiveColour;
     }
     ctx.beginPath();
     ctx.moveTo(canvasWidth - speed, (canvasHeight * zSettings.y) - (prevZLevel * zSettings.maxHeight * canvasHeight));
@@ -66,9 +75,9 @@ function drawCanvas(){
 
     // Draw line for xLevel
     if(xActive){
-        ctx.strokeStyle = "green";
+        ctx.strokeStyle = activeColour;
     } else {
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = inactiveColour;
     }
     ctx.beginPath();
     ctx.moveTo(canvasWidth - speed, (canvasHeight * xSettings.y) - (prevXLevel * xSettings.maxHeight * canvasHeight));
@@ -80,6 +89,12 @@ function drawCanvas(){
     prevXLevel = xLevel;
 }
 
+function updateKeyIcons(){
+    // style as a gradient background: linear-gradient(0deg, #00ff95 50%, #39393976 50%);
+    zKeyIcon.style.background = `linear-gradient(0deg, ${zActive ? activeColour : inactiveColour} ${zLevel * 100}%, ${transparentColour} ${zLevel * 100}%)`;
+    xKeyIcon.style.background = `linear-gradient(0deg, ${xActive ? activeColour : inactiveColour} ${xLevel * 100}%, ${transparentColour} ${xLevel * 100}%)`;
+}
+
 function startCanvas(){
     console.log("Starting canvas");
     runCanvas = true;
@@ -87,6 +102,7 @@ function startCanvas(){
         if(runCanvas){
             shiftCanvas(speed);
             drawCanvas();
+            updateKeyIcons();
         }
     }, 1000/120);
 }
